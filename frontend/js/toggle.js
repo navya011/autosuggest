567997883e21321fb0be494d11c6de0b19f412d0
navya <navyasushmasrialla@gmail.com>
2026-06-20 11:32:13 +0530

    Git Pulse

--- frontend/js/toggle.js
+++ frontend/js/toggle.js
@@ -14,7 +14,6 @@ var users = [
 var curId = 0;
 function toggle(){
     curId = (curId + 1) % 2;
-
     document.getElementById("user-img").src = users[curId].image;
     document.getElementById("user-name").innerText = users[curId].name;
     document.getElementById("user-gender").innerText = users[curId].gender;
