360af1219409a8b43f5db426c55e5a2adebbec94
navya <navyasushmasrialla@gmail.com>
2026-06-20 10:32:12 +0530

    Random Users

---
+++ frontend/js/toggle.js
@@ -0,0 +1,21 @@
+var users = [
+    {
+        "name": "John Doe",
+        "gender": "Male",
+        "image": "../images/john.png"
+    },
+    {
+        "name": "Jane Doe",
+        "gender": "Female",
+        "image": "../images/jane.png"
+    }
+]
+
+var curId = 0;
+function toggle(){
+    curId = (curId + 1) % 2;
+
+    document.getElementById("user-img").src = users[curId].image;
+    document.getElementById("user-name").innerText = users[curId].name;
+    document.getElementById("user-gender").innerText = users[curId].gender;
+}
